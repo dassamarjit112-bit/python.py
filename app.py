ce5b915b77a42bc14593c60044ed1051323aaa05
dassamarjit112-bit <dassamarjit112@gmail.com>
2026-04-06 04:54:58 +0530

    qr code

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# This route displays the home page
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    greeting = ""
+    
+    # Check if the user submitted the form
+    if request.method == 'POST':
+        # Get the input data from the HTML form (linked by 'name="username"')
+        user_input = request.form.get('username')
+        
+        if user_input:
+            greeting = f"Hello, {user_input}! Python processed this input."
+            
+    # Send the greeting variable back to the HTML page
+    return render_template('index.html', output=greeting)
+
+if __name__ == '__main__':
+    app.run(debug=True)
