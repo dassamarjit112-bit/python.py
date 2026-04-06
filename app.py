6cc0e710b35f3c6f59cd46ab565ca21ccfc4f80b
dassamarjit112-bit <dassamarjit112@gmail.com>
2026-04-06 06:25:36 +0530

    444

--- app.py
+++ app.py
@@ -4,14 +4,19 @@ from flask import Flask, render_template, request, send_file
 app = Flask(__name__)
 # This route displays the home page
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def index():
     qr_img_url = None
     # Check if the user submitted the form
     if request.method == 'POST':
         # Get the input data from the HTML form (linked by 'name="username"')
-        user_input = request.form.get('username')
+        user_input = request.form.get("username")
         if user_input:
-            img = qrcode.make(user_input)
+            #genarate qrcode 
+            qr = qrcode.Qrcode(version=1, box_size=10, border=5)
+            qr.add_data(user_input)
+            qr.make(fit=True)
+            #save img in memory
+            img = qr.make_image(fill_color="black", back_color="white")
             buffer = io.BytesIO()
             img.save(buffer, format="PNG")
             buffer.seek(0)
